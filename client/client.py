2457e79f0bd0402d448c6191a5505a998056c735
Micah Fitzgerald <mcfitz2@gmail.com>
2017-12-12 01:50:10 +0000

    rewrote client in python

---
+++ client/client.py
@@ -0,0 +1,99 @@
+import paho.mqtt.client as mqtt #import the client1
+import uuid
+import json
+import os
+import logging
+import datetime
+import time
+import schedule
+logging.basicConfig(level=logging.DEBUG)
+class Config(dict):
+	def __init__(self, path="config.json"):
+		self.path = path
+		if os.path.exists(self.path):
+			with open(self.path, 'r') as config_file:
+				self.update(json.load(config_file))
+		else:
+			self.update({
+				"broker_address":"micahf.com",
+				"port":1882,
+				"id":str(uuid.uuid4())
+			})
+			self.save()
+	def save(self):
+		with open(self.path, 'w') as config_file:
+			json.dump(self, config_file)
+class Client(mqtt.Client):
+	def __init__(self, config):
+		mqtt.Client.__init__(self, config["id"])
+		self.config = config
+		self.connect(self.config["broker_address"], self.config["port"]) #connect to broker
+		#self.on_message = on_message
+		self.subscribe("/feeder/" + self.config["id"] + "/feed")
+		self.subscribe("/feeder/" + self.config["id"] + "/schedules/set")
+		self.subscribe("/feeder/" + self.config["id"] + "/schedules/unset")
+		self.subscribe("/feeder/identify")
+		logging.info("Client started, ID = %s" % self.config["id"])
+	def start(self):
+		self.setup_schedules()
+		self.loop_start()
+		while True:
+			try:
+				schedule.run_pending()
+				time.sleep(1)
+			except KeyboardInterrupt:
+				self.loop_stop()
+				break
+	def heartbeat(self):
+		logging.debug("Sending Heartbeat %s" % self.config)
+		if schedule.default_scheduler.next_run:
+			self.config["nextFeeding"] = schedule.default_scheduler.next_run.isoformat()
+		self.publish("/feeder/" + self.config["id"] + "/heartbeat", json.dumps(self.config))
+	def feed(self, cups):
+		logging.info("Dispensing %d cups" % cups)
+	def setup_schedules(self):
+
+		schedule.clear("feedings")
+		if "schedules" in self.config:
+			for schedule_id, sched in self.config["schedules"].iteritems():
+				schedule.every().day.at("%s:%s" % (sched["hour"], sched["minute"])).do(self.feed, sched["cups"]).tag("feedings")
+	def on_message(self, client, userdata, message):
+		try:
+			payload = json.loads(message.payload)
+		except Exception as e:
+			logging.error(str(e))
+			logging.error("Payload parsing failed. Payload = %s" % message.payload)
+			return
+		if message.topic == "/feeder/identify":
+			self.heartbeat()
+		elif message.topic == "/feeder/%s/schedules/set" % self.config["id"]:
+			if not "schedules" in self.config:
+				logging.debug("Creating schedules object")
+				self.config["schedules"] = {}
+			if payload["schedule"]["deleted"]:
+				logging.info("Deleting schedule: %s" % self.config["schedules"][payload["schedule"]["id"]])
+				del self.config["schedules"][payload["schedule"]["id"]]
+			del payload["schedule"]["deleted"]
+			logging.info("Setting Schedule: %s" % payload["schedule"])
+			self.config["schedules"][payload["schedule"]["id"]] = payload["schedule"]
+			self.config.save()
+			self.setup_schedules()
+			self.heartbeat()
+		elif message.topic == "/feeder/%s/schedules/unset" % self.config["id"]:
+			logging.info("Deleting schedules: %s" % payload["schedules"])
+			for schedule in payload["schedules"]:
+				del self.config["schedules"][schedule["id"]]
+
+		elif message.topic == "/feeder/" + self.config["id"] + "/feed":
+			self.feed(payload["cups"])
+			config["lastFeeding"] = datetime.datetime.now().isoformat()
+			self.publish("/feeders/" + self.config["id"] + "/feeding", json.dumps(self.config))
+			self.config.save()
+		else:
+			logging.info("Topic = %s, Message = %s" % (message.topic, payload))
+if __name__ == "__main__":
+	config = Config("config.json")
+	c = Client(config)
+	logging.info("Device Key is %s" % config["id"])
+	c.start()
+
